bc2f39cdfd5d472a5e7fb58807cea6ec7a5f2799
VasuRakholiya <89647248+VasuRakholiya@users.noreply.github.com>
2025-01-15 16:54:36 +0530

    commit this

--- 18 Fragments/learning-Fragments/src/components/Item.jsx
+++ 18 Fragments/learning-Fragments/src/components/Item.jsx
@@ -1,4 +1,6 @@
+import styles from "./Item.module.css"
+
 const Item = ({foodItem}) => {
-  return <li className="list-group-item style" ><span className="Span">{foodItem}</span></li>;
+  return <li className={`${styles['style']} list-group-item`} ><span className={styles['Span']}>{foodItem}</span></li>;
 }
 export default Item;
